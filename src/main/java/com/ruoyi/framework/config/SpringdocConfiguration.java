6ce6bc669e5b18a633f800de7933b1c8999cdf87
DubaiJun <1533181938@qq.com>
2023-07-16 21:59:06 +0800

    第一次推送

---
+++ src/main/java/com/ruoyi/framework/config/SpringdocConfiguration.java
@@ -0,0 +1,53 @@
+package com.ruoyi.framework.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author <a href="mailto:huangbing0730@gmail">hb0730</a>
+ * @date 2023/1/12
+ */
+@Configuration
+public class SpringdocConfiguration {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(getInfo()
+                ).addSecurityItem(
+                        new io.swagger.v3.oas.models.security.SecurityRequirement()
+                                .addList("")
+                ).schemaRequirement(
+                        "name",
+                        securityScheme()
+                ).components(
+                        new Components()
+                                .addSecuritySchemes("BootAdminConst.X_ACCESS_TOKEN", securityScheme())
+                );
+    }
+
+    private Info getInfo() {
+        return new Info()
+                .title("boot-admin系统API")
+                .contact(
+                        new Contact()
+                                .name("hb0730")
+                                .url("https://blog.hb0730.me")
+                                .email("huangbing0730@gmail.com")
+                )
+                .version("v5.0.0")
+                ;
+    }
+
+    private SecurityScheme securityScheme() {
+        SecurityScheme securityScheme = new SecurityScheme();
+        securityScheme.setType(SecurityScheme.Type.APIKEY);
+        securityScheme.setName("BootAdminConst.X_ACCESS_TOKEN");
+        securityScheme.setIn(SecurityScheme.In.HEADER);
+        return securityScheme;
+    }
+}
